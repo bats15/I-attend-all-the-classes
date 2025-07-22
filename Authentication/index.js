102ff93211b0d9ca6802ab2c70906859240c49b8
Akshat Rai <2024uch0004@iitjammu.ac.in>
2025-07-23 01:18:33 +0530

    hi

---
+++ Authentication/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectMongoDB = require('./connection/connect');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
