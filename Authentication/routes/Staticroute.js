0c6fa128a4ec0a7add230969fb8d2358edfdcf2a
Basit Warsi <basitwarsi2595@gmail.com>
2025-07-23 01:36:14 +0530

    staticroutes mod

--- Authentication/routes/Staticroute.js
+++ Authentication/routes/Staticroute.js
@@ -1,1 +1,10 @@
-function
+function staticRoute(app){
+    app.get('/signup', (req, res) => {
+        res.render('signup');
+    });
+    app.get('/login', (req, res) => {
+        res.render('login');
+    });
+}
+
+module.exports = staticRoute;
