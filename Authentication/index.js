2e956615f6053ce8473dd8a1cca5c8bf66cd37f5
Akshat Rai <2024uch0004@iitjammu.ac.in>
2025-07-23 17:01:10 +0530

    completed backend of authentication

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const connectMongoDB = require('./connection/connect');
+const userRoutes = require('../Authentication/routes/user');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const staticRoutes = require('../Authentication/routes/Staticroute')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views',path.resolve('../Authentication/views'));
+
+connectMongoDB('mongodb://localhost:27017/attendance')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 
+    app.use('/user', userRoutes);
+    app.use('/', staticRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- Authentication/routes/user.js
+++ Authentication/routes/user.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {handleUserSignup, handleUserLogin} = require('../controller/user')
 
-router.post('/', handleUserSignup)
+router.post('/signup', handleUserSignup)
 router.post('/login',handleUserLogin )
-router.get('/demo', (req, res) =>{
-    return res.send("This is a demo route for user signup and login");
-})
+
 
 module.exports = router;
